ad76950cf3a00f567473304ef800d55cc758e51d
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 10:45:29 +0200

    appels composants

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Nav @click="showMenu = !showMenu"/>
+  <div class="home" v-if="showMenu">
+    <!-- <img alt="logo" src=""> -->
+    <div id="me">
+      <img id="portrait" src="./assets/images/me.jpg" alt="Photo de moi" />
+    </div>
+    <div id="projects" v-for="project in projects" :key="project.id">
+      <ProjectCard :project="project" />
+    </div>
+    <div id="contact">
+        <ContactMe />
+    </div>
+  </div>
 </template>
 
+<script>
+import Nav from "@/components/MenuBars.vue";
+import projects from "@/assets/data_projects";
+import ProjectCard from "@/components/ProjectCard.vue";
+import ContactMe from "@/components/ContactMe.vue";
+
+export default {
+  data() {
+    return {
+      projects,
+      showMenu: true
+    };
+  },
+  components: {
+    Nav, ProjectCard, ContactMe
+  },
+};
+</script>
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import "main.scss";
+.home {
+  display: flex;
+  flex-wrap: wrap;
+  border-radius: 1em;
+  justify-content: space-around;
+  gap: 1em 1.5em;
+}
+#me {
+  width: 100%;
+  display: flex;
+    justify-content: center
+}
+#portrait {
+  width: 40%;
+  border-radius: 50%;
+  // variable sass ne fonctionne pas
+  border: 2px solid $color-dark;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
