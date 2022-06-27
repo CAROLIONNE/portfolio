3c3c612ae7bf03264b0410b1f5fdd8b3ec970fa3
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 10:44:51 +0200

    supression fichier origin

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomePortfolio from '../views/HomePortfolio.vue'
 
 const routes = [
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // },
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'HomePortfolio',
+    component: HomePortfolio
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
 ]
 
 const router = createRouter({
