d1cbc7863a215398a68a5c2084a026ba1808c6d3
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 10:46:10 +0200

    composant contact

---
+++ src/components/ContactMe.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Contactez-moi</h1>
+  <h2>Envie de me presenter un projet ou tout simplement discuter ?</h2>
+  <form @submit.prevent="submit">
+      <input id="name" v-model="name" type="text" name="name" placeholder="Nom" />
+      <input id="mail" v-model="mail" type="email" name="mail" placeholder="Email" />
+      <input id="message" v-model="message" type="text" name="message" placeholder="Message" />
+      <input type="submit" value="Envoyer">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "ContactMe",
+  methods: {
+    submit() {},
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../main.scss";
+form {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border: 2px solid $color-dark;
+    border-radius: 1em;
+    input {
+      width: 80%;
+      padding: 0.5em;
+      margin: 0.5em;
+    }
+}
+</style>
