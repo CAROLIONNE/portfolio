cceb8a797a13f8876051a185ebd4e9bb510ebd85
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 14:39:21 +0200

    modif mineures

--- src/assets/data_projects.js
+++ src/assets/data_projects.js
@@ -2,7 +2,7 @@ const projects = [
     { id: 1, developpement: "Front-end", title: "Site web responsive" , description: "Réservation d'hébergements et d'activités", logo: '../../logos/reservia.png', img:"" , type: "cours", techno:["HTML5", "CSS"]},
     { id: 2, developpement: "Front-end", title: "Site web responsive avec animations CSS" , description: "Réservation restaurants, Présentation des menus", logo: './logos/ohmyfood.png', img:"" , type: "cours", techno:["HTML5", "SASS"]},
     { id: 3, developpement: "Front-end", title: "Site web responsive" , description: "Vitrine et boutique en ligne", logo: '../logos/kanap.png', img:"" , type: "cours", techno:["JAVASCRIPT"]},
-    { id: 4, developpement: "Back-end", title: "Application sécurisée" , description: "Critique de sauces piquantes", logo: './logos/piiquante.png', img:"" , type: "cours", techno:["NODE JS", "JAVASCRIPT", "MONGODB"]},
+    { id: 4, developpement: "Back-end", title: "Application sécurisée" , description: "Critique de sauces piquantes",logo: './logos/piiquante.png', img:"" , type: "cours", techno:["NODE JS", "JAVASCRIPT", "MONGODB"]},
     { id: 5, developpement: "Full-stack", title: "Site web responsive / Base de donnée / Serveur" , description: "Réseau social d'entreprise", logo: './logos/groupomania.png', img:"" , type: "cours", techno:["JAVASCRIPT", "VUE", "NODE JS", "SQL", "SEQUELIZE"]},
 ]
 export default projects
