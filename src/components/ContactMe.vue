4785ea003a3d9a51001776cc1d8e2ea9afe07259
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 16:17:17 +0200

    display

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,12 +1,31 @@
 <template>
-  <h1>Contactez-moi</h1>
-  <h2>Envie de me presenter un projet ou tout simplement discuter ?</h2>
-  <form @submit.prevent="submit">
-      <input id="name" v-model="name" type="text" name="name" placeholder="Nom" />
-      <input id="mail" v-model="mail" type="email" name="mail" placeholder="Email" />
-      <input id="message" v-model="message" type="text" name="message" placeholder="Message" />
-      <input type="submit" value="Envoyer">
-  </form>
+  <div>
+    <h3>Envie de me presenter un projet ou tout simplement discuter ?</h3>
+    <form @submit.prevent="submit">
+      <input
+        id="name"
+        v-model="name"
+        type="text"
+        name="name"
+        placeholder="Nom"
+      />
+      <input
+        id="mail"
+        v-model="mail"
+        type="email"
+        name="mail"
+        placeholder="Email"
+      />
+      <input
+        id="message"
+        v-model="message"
+        type="text"
+        name="message"
+        placeholder="Message"
+      />
+      <input type="submit" value="Envoyer" />
+    </form>
+  </div>
 </template>
 
 <script>
@@ -20,16 +39,22 @@ export default {
 
 <style lang="scss" scoped>
 @import "../main.scss";
+div {
+  margin-left: 3em;
+}
 form {
-    display: flex;
-    align-items: center;
-    flex-direction: column;
-    border: 2px solid $color-dark;
-    border-radius: 1em;
-    input {
-      width: 80%;
-      padding: 0.5em;
-      margin: 0.5em;
-    }
+  width: 80%;
+    margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  border: 2px solid $color-dark;
+  border-radius: 1em;
+  input {
+    width: 80%;
+    padding: 0.5em;
+    margin: 0.5em;
+  }
 }
 </style>
