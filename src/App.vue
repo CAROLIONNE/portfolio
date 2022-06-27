d7c99d1b9db00e4a07da5ffce4b02f9cc776b92f
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 17:59:11 +0200

    ajout de container

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <Nav @click="showMenu = !showMenu"/>
+  <Nav @click="showMenu = !showMenu" />
   <div class="home" v-if="showMenu">
     <!-- <img alt="logo" src=""> -->
     <div id="me">
       <img id="portrait" src="./assets/images/me.jpg" alt="Photo de moi" />
     </div>
-    <div id="projects" v-for="project in projects" :key="project.id">
-      <ProjectCard :project="project" />
+    <div id="container-skills">
+      <h2 id="skills">Compétences</h2>
+      <!-- ajouter logo technos  -->
     </div>
-    <div id="contact">
-        <ContactMe />
+    <div id="container-projects">
+      <h2 id="portfolio">Portfolio</h2>
+      <div id="projects" v-for="project in projects" :key="project.id">
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+    <div id="container-contact">
+      <h2 id="contact">Contactez-moi</h2>
+      <ContactMe />
     </div>
   </div>
 </template>
@@ -24,33 +32,68 @@ export default {
   data() {
     return {
       projects,
-      showMenu: true
+      showMenu: true,
     };
   },
   components: {
-    Nav, ProjectCard, ContactMe
+    Nav,
+    ProjectCard,
+    ContactMe,
   },
 };
 </script>
 <style lang="scss">
 @import "main.scss";
-.home {
-  display: flex;
-  flex-wrap: wrap;
-  border-radius: 1em;
-  justify-content: space-around;
-  gap: 1em 1.5em;
-}
+// .home {
+//   display: flex;
+//   flex-wrap: wrap;
+//   border-radius: 1em;
+//   justify-content: space-around;
+//   gap: 1em 1.5em;
+// }
 #me {
   width: 100%;
   display: flex;
-    justify-content: center
+  justify-content: center;
+  height: 500px;
 }
 #portrait {
-  width: 40%;
-  border-radius: 50%;
-  // variable sass ne fonctionne pas
+  //   margin-left: auto;
+  // margin-right: auto;
+  display: flex;
+  max-width: 100%;
+  max-height: 100%;
+  border-radius: 25%;
   border: 2px solid $color-dark;
 }
-
+#container-skills {
+  #skills {
+    width: 100%;
+    margin-left: 2em;
+  }
+}
+#container-projects {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 1em 1.5em;
+  #portfolio {
+    // a perso cursor
+    cursor: pointer;
+    width: 100%;
+    margin-left: 2em;
+    transition: all 1s;
+    // effet a tester
+    &:hover {
+      border-bottom: 2px solid $color-primary;
+    }
+  }
+}
+#container-contact {
+  #contact {
+    width: 100%;
+    margin-left: 2em;
+  }
+}
 </style>
