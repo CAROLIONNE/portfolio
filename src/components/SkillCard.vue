28a852c46005c98c97a6c3ef846c3bd2e786957d
soc caroline <soc.caroline1991@gmail.com>
2022-06-27 16:16:59 +0200

    init card skill

---
+++ src/components/SkillCard.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="container">
+        <div id="card" >
+            <img :src="skill.logo"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'SkillCard',
+  props: {
+    skill: Object,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../main.scss";
+// #container {
+//   border: 2px solid $color-dark;
+//   border-radius: 1em;
+//   width: 400px;
+//   background:	$color-tertiary;
+//   overflow: hidden;
+//   padding: 0.5em
+// }
+
+</style>
